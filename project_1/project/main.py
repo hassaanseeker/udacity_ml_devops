d56d98ea4e0b8ec871c43ecb57f69ed18792b1be
Muhammad Hassaan Rafique <muhammadhassaanrafique@Muhammads-MacBook-Pro.local>
2021-11-22 21:49:36 +0400

    udacity machine-learning devops project

---
+++ project_1/project/main.py
@@ -0,0 +1,22 @@
+from churn_library import ChurnPredictor
+
+import warnings
+warnings.filterwarnings("ignore")
+
+if __name__ == '__main__':
+
+    cat_columns = [
+    'Gender',
+    'Education_Level',
+    'Marital_Status',
+    'Income_Category',
+    'Card_Category'                
+]
+
+    
+    
+
+    churn_predictor = ChurnPredictor(pth=r"./data/bank_data.csv")
+
+    # print(churn_predictor.import_data())
+    churn_predictor.classification_report_image()
